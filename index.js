5945dd6321a0a9d153a45c0498a5f4955650ee25
Arnaud Ruant <external.z31aruan@decathlon.com>
2018-01-22 17:22:40 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,64 @@
+Vue.use(VueFusionCharts);
+
+var myDataSource = {
+  "chart": {
+        "caption": "Compétences",
+        "subcaption": "",
+        "xAxisMinValue": "0",
+        "xAxisMaxValue": "100",
+        "yAxisMinValue": "0",
+        "yAxisMaxValue": "100",
+        "showYAxisValues": "0",
+        "showToolTip": "0",
+        "plotFillAlpha": "70",
+        "plotFillHoverColor": "#6baa01",
+        "xAxisName": "Acquis ->",
+        "yAxisName": "Intérêt ->",
+        "numDivlines": "0",
+        "showValues": "1",
+        "drawQuadrant": "1",
+        "quadrantLabelTL": "ENTHOUSIASTE",
+        "quadrantLabelTR": "FERVENT",
+        "quadrantLabelBL": "CURIEUX",
+        "quadrantLabelBR": "EXPERIMENTÉ",
+        "bubbleScale": "0.5"
+    },
+    "categories": [{
+        "category": [{
+            "x": "0"
+        }, {
+            "x": "100"
+        }]
+    }]
+};
+
+function prettyPrint() {
+  var ugly = document.getElementById('myTextArea').value;
+  var obj = JSON.parse(ugly);
+  var pretty = JSON.stringify(obj, undefined, 4);
+  document.getElementById('myTextArea').value = pretty;
+}
+
+const chart = new Vue({
+  el: '#app',
+  data: {
+    type: 'bubble',
+    width: '1000', //to specify the width of the chart
+    height: '600', //to specify the height of the chart
+    dataFormat: 'json',
+    dataSource: {},
+    dataSet: '[{"color": "#00aee4","data":[{"x": "85","y": "90","z": "10","name": "Git"}]}]'
+  },
+  mounted () {
+    //prettyPrint();
+    this.dataSource = Object.assign({}, myDataSource);
+  },
+  methods: {  
+    updateSkills() {
+
+      this.dataSource.dataset = JSON.parse(this.dataSet);
+
+      console.log(JSON.stringify(this.dataSource));
+    }
+  }
+});
