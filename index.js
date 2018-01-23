ce25b188166703b0479427519f8fd70bce7f0e98
Arnaud Ruant <external.z31aruan@decathlon.com>
2018-01-23 17:49:21 +0100

    feat : xy click position

--- index.js
+++ index.js
@@ -30,25 +30,59 @@ var myDataSource = {
             "x": "100"
         }]
     }],
-    "dataset":[{}]
+    "dataset":[{"color": "#00aee4","data":[{"x": "85","y": "90","z": "10","name": "Git"}]}]
 };
 
-function prettyPrint() {
-  var ugly = document.getElementById('myTextArea').value;
-  var obj = JSON.parse(ugly);
-  var pretty = JSON.stringify(obj, undefined, 4);
-  document.getElementById('myTextArea').value = pretty;
+var defaultColors = {
+  hex: '#194d33',
+  hsl: {
+    h: 150,
+    s: 0.5,
+    l: 0.2,
+    a: 1
+  },
+  hsv: {
+    h: 150,
+    s: 0.66,
+    v: 0.30,
+    a: 1
+  },
+  rgba: {
+    r: 25,
+    g: 77,
+    b: 51,
+    a: 1
+  },
+  a: 1
 }
 
-const chart = new Vue({
+var compact = VueColor.Compact
+
+var chart = new Vue({
   el: '#app',
+  components: {
+    'compact-picker': compact
+  },
   data: {
     type: 'bubble',
     width: '1000', //to specify the width of the chart
     height: '600', //to specify the height of the chart
     dataFormat: 'json',
     dataSource: {},
-    dataSet: ''
+    events: {
+      chartClick: function (eventObj, dataObj) {
+        // 35 -> 0 ; 980 -> 100
+        // (980-35)/100 = 9.45
+        console.log((dataObj.chartX - 35) / 9.45);
+        // 560 -> 0 ; 40 -> 100
+        // (40-560)/100 = -5.2
+        console.log((dataObj.chartY - 560) / -5.2);
+
+        //this.$refs.myModalRef.show();
+      }   
+    },
+    dataSet: '',
+    colors: defaultColors
   },
   mounted () {
     //prettyPrint();
@@ -60,3 +94,16 @@ const chart = new Vue({
     }
   }
 });
+
+
+function prettyPrint() {
+  var ugly = document.getElementById('myTextArea').value;
+  var obj = JSON.parse(ugly);
+  var pretty = JSON.stringify(obj, undefined, 4);
+  document.getElementById('myTextArea').value = pretty;
+}
+
+$('.modal-content').draggable();
+
+
+
